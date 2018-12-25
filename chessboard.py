63a6110505ddef7c5c8353e65478b10a725e53a8
marc <marc@insigtness.com>
2018-12-25 16:40:53 +0100

    chessboad reads correct positions (binary)

--- chessboard.py
+++ chessboard.py
@@ -28,30 +28,40 @@ class Chessboard:
         self._rowCD = MCP23017(1) # Device address 1
         self._rowEF = MCP23017(2) # Device address 2
         self._rowGH = MCP23017(3) # Device address 3
-        self._positions = [0]*8
+        self._positions = [[0]*8]*8
 
         # register interrupt callback
         GPIO.add_event_detect(self._interrupt_pin, GPIO.FALLING, callback=self._onInterruptEvent, bouncetime=10)
         self.readPositions() # clear interrupt
 
     def _onInterruptEvent(self, channel):
-        print(self.readPositions())
+        pos = self.readPositions()
+        print("\n")
+        print(pos[7])
+        print(pos[6])
+        print(pos[5])
+        print(pos[4])
+        print(pos[3])
+        print(pos[2])
+        print(pos[1])
+        print(pos[0])
 
     def getLastPositions(self):
         return self._positions
 
     def readPositions(self):
-        self._positions[0:2] = self._rowAB.readAll()
-        self._positions[2:4] = self._rowCD.readAll()
-        self._positions[4:6] = self._rowEF.readAll()
-        self._positions[6:8] = self._rowGH.readAll()
+        pos_int = [0]*8
+        pos_int[6:8] = self._rowAB.readAll()[::-1]
+        pos_int[4:6] = self._rowCD.readAll()[::-1]
+        pos_int[2:4] = self._rowEF.readAll()[::-1]
+        pos_int[0:2] = self._rowGH.readAll()[::-1]
+        i = 0
+        for val in pos_int:
+            self._positions[i] = [int(x) for x in "{0:08b}".format(val)]
+            if i == 1 or i == 3 or i == 5 or i == 7:
+                self._positions[i] = self._positions[i][::-1]
+            i += 1
         return self._positions
-
-def test_callback(channel, device):
-    print(self._rowAB.readAll())
-    print(self._rowCD.readAll())
-    print(self._rowEF.readAll())
-    print(self._rowGH.readAll())
 
 if __name__ == '__main__':
     chessboard = Chessboard()
