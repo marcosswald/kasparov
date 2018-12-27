abe6ebfbced204d76ac672c0ee6fa6a59de07933
marc <marc@insigtness.com>
2018-12-27 17:42:58 +0100

    basic flask-ask framework

--- kasparov.py
+++ kasparov.py
@@ -1,12 +1,21 @@
-import time
+from flask import Flask
+from flask_ask import Ask, statement, question, session
 from chessboard import Chessboard
 
 # create chess board
-chessboard = Chessboard()
+# chessboard = Chessboard()
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@app.route('/')
+def homepage():
+    return "Hi there, kasparov is up and running"
+
+@ask.intent('BestMoveIntent')
+def best_move():
+    return statement("Kasparov says that you should move your king.")
 
 # main
 if __name__ == '__main__':
-    # main loop
-    while True:
-        
-        time.sleep(2)
+    app.run(debug=True)
